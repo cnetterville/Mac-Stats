f176e882aec352c68c5bcabeaf539fdb65307352
Curtis Netterville <curtis@netterville.com>
2025-08-29 07:23:51 -0500

    Initial commit for checkpoints

---
+++ Mac Stats/Mac_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_StatsApp.swift
+//  Mac Stats
+//
+//  Created by Curtis Netterville on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Mac_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mac StatsTests/Mac_StatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_StatsTests.swift
+//  Mac StatsTests
+//
+//  Created by Curtis Netterville on 8/29/25.
+//
+
+import Testing
+@testable import Mac_Stats
+
+struct Mac_StatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
